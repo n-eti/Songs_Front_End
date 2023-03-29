926ad9af1ffbc84a16d0314b64db84c16cb7a008
Nada Atieh <nada.s.atieh@gmail.com>
2023-03-29 02:25:13 -0500

    Used useState and useEffect in App.jsx to fetch songs + Created table in the Music Table component to display songs + Instantiated Music Table component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import MusicTable from './Components/MusicTable/MusicTable';
 function App() {
+  const [songs, setSongs] = useState([])
 
-  function fetchSongs(){
-    const response = axios.get('http://127.0.0.1:8000/api/music/');
+ async function fetchSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data)
+    console.log("Music Data", response.data)
   }
+
+  useEffect(() => {
+    fetchSongs()
+  }, []);
   return (
     <div className="App">
-
+        {console.log('Songs to be rendered', songs)}
+        <MusicTable songs={songs}/>
     </div>
   );
 }
--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
+
 const MusicTable = (props) => {
     return (
-        <table>
-            
-        </table>
-    );
+     <table>
+    <tr>
+     <th>Title</th>
+     <th>Artist</th>
+     <th>Album</th>
+     <th>Release Date</th>
+     <th>Genre</th>
+
+      </tr>
+       {props.songs.map((songs) =>{
+        return(
+            <tr>
+            <td>{songs.title}</td>
+            <td>{songs.artist}</td>
+            <td>{songs.album}</td>
+            <td>{songs.release_date}</td>
+            <td>{songs.genre}</td>
+            </tr>
+        )
+       })}
+
+
+</table>
+  );
 }
- 
+
 export default MusicTable;
